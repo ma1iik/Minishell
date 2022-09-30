eeb5d116c86dd4ffc2b5edc4113dccca12dd5c26
Malik Israilov <misrailo@c4r1p3.42nice.fr>
2022-09-30 23:47:16 +0200

    thank u asma

--- main.c
+++ main.c
@@ -6,16 +6,26 @@
 /*   By: misrailo <misrailo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 19:03:06 by misrailo          #+#    #+#             */
-/*   Updated: 2022/09/26 19:10:56 by misrailo         ###   ########.fr       */
+/*   Updated: 2022/09/30 23:30:49 by misrailo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <>
-#include <>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-int main(int ac, char **av)
+void read_line(void)
+{
+	char *line;
+	while (1)
+	{
+		line = readline("minishell:");
+	}
+}
+
+int main(void)
 {
     read_line();
-    parse_line();
-    execute_line();
+    // parse_line();
+    // execute_line();
 }
